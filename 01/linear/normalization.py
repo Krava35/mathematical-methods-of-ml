69d46991724d3cfedadb3fbe658bc0a0e40a0498
Daniel Kravchenko <59441574+Krava35@users.noreply.github.com>
2024-10-21 20:18:08 +0300

    add chages to kernel methods

--- 01/linear/normalization.py
+++ 01/linear/normalization.py
@@ -7,24 +7,18 @@ from .matrix import Matrix
 def normalization(matrix: Matrix, degree: int = 2) -> Matrix:
     distances = []
     for i in range(matrix.shape[0]-1):
-        for j in range(matrix.shape[1]-i-1):
-            distances.append(__normalization(matrix.value[i], matrix.value[j+i+1], degree))
+        distances.append(__normalization(matrix.value[i], degree))
     return __transformation(distances, matrix.shape[1])
 
 
-def __normalization(array1: npt.ArrayLike, array2: npt.ArrayLike, degree: int = 2) -> np.float64:
-    return (np.sum((array1 - array2) ** degree)) ** (1 / degree)
+def __normalization(array: npt.ArrayLike, degree: int = 2) -> np.float64:
+    return (np.sum(np.abs(array) ** degree)) ** (1 / degree)
 
 
 def __transformation(distances: List, n: int) -> Matrix:
     n = len(distances)
-
-    if n == 1:
-        return Matrix(np.array(distances))
-
+    
     m = np.zeros((n, n), dtype=np.float64)
     for i in range(n):
-        for j in range(n):
-            if i != j:
-                m[i, j] = distances[i] if i < j else distances[j]
+        m[i] = distances[-i:] + distances[:-i]
     return Matrix(m)
--- 01/models/pca.py
+++ 01/models/pca.py
@@ -14,28 +14,20 @@ class PCA:
 
     def __rbf_kernel(self, matrix: Matrix, gamma: np.float64 | None = None):
         if not gamma:
-            gamma = 1 / matrix.shape(0)
+            gamma = 1 / matrix.shape[0]
         norm_matrix = normalization(matrix, degree=2)
-        return np.exp(-gamma * norm_matrix.value)
+        return np.exp(-gamma * (norm_matrix.value * norm_matrix.value - 2 * matrix.transpose().value() * matrix.value()))
 
     def __laplacian_kernel(self, matrix: Matrix, gamma: np.float64 | None = None):
         if not gamma:
-            gamma = 1 / matrix.shape(0)
+            gamma = 1 / matrix.shape[0]
         norm_matrix = normalization(matrix, degree=1)
         return np.exp(-gamma * norm_matrix.value)
 
-    def __poly_kernel(self, matrix: Matrix, degree: np.float64 = 3, gamma: np.float64 | None = None, c: np.float64 | None = None):
-        if not gamma:
-            gamma = 1 / matrix.shape(0)
-        if not c:
-            c = 1
+    def __poly_kernel(self, matrix: Matrix, degree: np.float64 = 3, gamma: np.float64 | None = 1, c: np.float64 | None = 1):
         coeff = np.full(matrix.shape, c)
         return (gamma * matrix.transpose().value() * matrix.value() + coeff) ** degree
     
-    def __sigmoid_kernel(self, matrix: Matrix, gamma: np.float64 | None = None, c: np.float64 | None = None):
-        if not gamma:
-            gamma = 1 / matrix.shape(0)
-        if not c:
-            c = 1
+    def __sigmoid_kernel(self, matrix: Matrix, gamma: np.float64 | None = 1, c: np.float64 | None = 1):
         coeff = np.full(matrix.shape, c)
         return np.tanh(gamma * matrix.transpose().value() * matrix.value() + coeff)
